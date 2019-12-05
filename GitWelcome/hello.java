a49628b252b1276245ff548bda0a575bc25f5ec6
May <May@LAPTOP-RQ1TCAAN.Home>
2019-12-05 10:38:14 +0200

    added my name

--- GitWelcome/hello.java
+++ GitWelcome/hello.java
@@ -4,6 +4,6 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
-		System.out.println("My name is May. ");
+		System.out.println("My name is May.");
 	}
 }
